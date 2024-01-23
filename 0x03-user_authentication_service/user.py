82ef0597ac22a653e86b02ad5047c88ffb164960
Ajibade Aanuoluwa James <ajibadejames19@gmail.com>
2024-01-23 18:57:32 +0100

    user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,14 +14,6 @@ class User(Base):
     """
     User class - Represents the 'users' table in the database.
 
-    Attributes:
-    - id (int): The integer primary key.
-    - email (str): A non-nullable string representing the email.
-    - hashed_password (str): A non-nullable string representing the hashed password.
-    - session_id (str): A nullable string representing the session ID.
-    - reset_token (str): A nullable string representing the reset token.
-    """
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
